c7f9138f8027d9422f837ee43353b3eac83c99c5
Riviere King <riviere.king@outlook.com>
2022-01-28 21:51:55 -0400

    revert interface move

--- src/controllers/AbstractState.ts
+++ src/controllers/AbstractState.ts
@@ -1,5 +1,10 @@
 import { IStateLink } from './StateLink'
 
+export interface IAbstractState {
+  /** Spreadable props */
+  [props: string]: any
+}
+
 export interface IAbstractStateDrawer {
   /** List of icons with the descriptions */
   items?: IStateLink[],
