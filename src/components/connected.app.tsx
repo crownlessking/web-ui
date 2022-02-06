d663eb7f08f620ee91683631eac39fa2bf1ffe0a
Riviere King <riviere.king@outlook.com>
2022-02-06 19:33:34 -0400

    removed some irrelevant jss

--- src/components/connected.app.tsx
+++ src/components/connected.app.tsx
@@ -1,9 +1,6 @@
 import { Component, Fragment } from 'react'
-import { CssBaseline, Theme } from '@mui/material'
+import { CssBaseline } from '@mui/material'
 import { connect } from 'react-redux'
-import {
-  createStyles, withStyles, WithStyles
-} from '@mui/styles'
 import { RootState } from '../state'
 import Navigation from './navigation.component'
 import Layout from './layout.component'
@@ -18,30 +15,13 @@ import StatePage from '../controllers/StatePage'
 import { postReqState } from '../state/net.controller'
 import { getBootstrapKey } from '../state/app.controller'
 
-// https://material-ui.com/guides/typescript/#usage-of-withstyles
-const styles = ({ spacing }: Theme) => createStyles({
-  root: {
-    width: '100%',
-    // display: 'flex',
-    height: 'inherit',
-  },
-  rightIcon: {
-    marginLeft: spacing(1),
-  },
-  textField: {
-    marginLeft: spacing(1),
-    marginRight: spacing(1),
-    width: 300,
-  },
-})
-
 const mapStateToProps = (state: RootState) => ({ state })
 
 const mapDispatchToProps = {
   onPostReqState: postReqState
 }
 
-interface IAppProps extends WithStyles<typeof styles> {
+interface IAppProps {
   state: RootState
   onPostReqState: typeof mapDispatchToProps['onPostReqState']
 }
@@ -75,21 +55,18 @@ class App extends Component<IAppProps> {
     this.root = new State(this.props.state)
     const page = this.root.allPages.getPage()
     this.pageID = page._id
-    const { classes } = this.props
 
     page.setTabTitle()
 
     return (
       <Fragment>
         <Background def={page.background} />
-        <div className={classes.root}>
-          <CssBaseline />
-          <Navigation def={page} />
-          <Drawer def={page} />
-          <Layout def={page}>
-            <Content def={page} />
-          </Layout>
-        </div>
+        <CssBaseline />
+        <Navigation def={page} />
+        <Drawer def={page} />
+        <Layout def={page}>
+          <Content def={page} />
+        </Layout>
         <Dialog pageDef={page} />
         <Snackbar />
         <Spinner />
@@ -102,4 +79,4 @@ class App extends Component<IAppProps> {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(withStyles(styles)(App as any))
+)(App as any)
